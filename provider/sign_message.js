d35ba8110e39173a5f25a97fb48196a6412698b9
ketan <shubhketanagarwal@99ProblemsButWiFiAint1.local>
2023-07-18 16:45:58 +0530

    update code to include paymaster in msg

--- provider/sign_message.js
+++ provider/sign_message.js
@@ -1,8 +1,7 @@
-const {ethers} = require("ethers");
+const {ethers, utils} = require("ethers");
 const {arrayify, keccak256} = require("ethers/lib/utils");
 
-
-const privateKey = '<your private key>';
+const privateKey = 'private_key';
 const providerUrl = 'http://localhost:8545';
 const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 const wallet = new ethers.Wallet(privateKey, provider);
@@ -10,53 +9,65 @@ const wallet = new ethers.Wallet(privateKey, provider);
 const userOpType = ['address', 'uint256', 'bytes32', 'bytes32',
     'uint256', 'uint256', 'uint256', 'uint256', 'uint256',
     'bytes32']
-
-let userOperation = {
-    sender: '<SCW address>',
-    nonce: '0',
-    initCode: '0x',
-    callData: '<generated calldata>',
-    callGasLimit: 10000000,
-    verificationGasLimit: 10000000,
-    preVerificationGas: 1000000000000000,
-    maxFeePerGas: 5,
-    maxPriorityFeePerGas: 1000000000,
-    paymasterAndData: '0x',
-    signature: '0x'
-}
-const message = arrayify(getUserOpHash(userOperation, '<entry point>', 31337))
-
-function getUserOpHash(op, entryPoint, chainId) {
-    const userOpHash = keccak256(packUserOp(op))
-    const enc = ethers.utils.defaultAbiCoder.encode(
-        ['bytes32', 'address', 'uint256'],
-        [userOpHash, entryPoint, chainId])
-    return keccak256(enc)
+const MOCK_VALID_UNTIL = '0x00000000deadbeef'
+const MOCK_VALID_AFTER = '0x0000000000001234'
+async function getVerifyingPaymasterData(paymaster) {
+    let now = await provider.getBlock('latest').then((block) => block.timestamp)
+    const timeRange = ethers.utils.defaultAbiCoder.encode(['uint48', 'uint48'], [MOCK_VALID_UNTIL, MOCK_VALID_AFTER])
+    // return ethers.utils.hexConcat([paymaster, timeRange])
+    console.log('timeRange: ' + timeRange.length)
+    console.log('timeRange: ' + timeRange)
+    return utils.hexConcat([paymaster, timeRange, '0x' + '00'.repeat(65)])
 }
 
-function packUserOp(op) {
-    return ethers.utils.defaultAbiCoder.encode(
-        userOpType,
-        [op.sender, op.nonce, keccak256(op.initCode), keccak256(op.callData),
-            op.callGasLimit, op.verificationGasLimit, op.preVerificationGas, op.maxFeePerGas, op.maxPriorityFeePerGas,
-            keccak256(op.paymasterAndData)])
-}
+getVerifyingPaymasterData('<paymaster_address_or_0x>').then((data) => {
 
-wallet.signMessage(message).then((sig) => {
-    userOperation.signature = sig
-    console.log(userOperation)
-
-    let values = Object.values(userOperation)
-    let str = ""
-    for (let i = 0; i < values.length; i++) {
-        str += values[i]
-        if (i < values.length - 1) {
-            str += ","
-        }
+    let userOperation = {
+        sender: '0x418fe523f35f6b094ffe3e8ed45db30840a24431',
+        nonce: '0',
+        initCode: '0x',
+        callData: '<calldata>',
+        callGasLimit: 10000000,
+        verificationGasLimit: 10000000,
+        preVerificationGas: 1000000000000000,
+        maxFeePerGas: 5,
+        maxPriorityFeePerGas: 1000000000,
+        paymasterAndData: data,
+        signature: '0x'
     }
-    console.log(str)
+    const message = arrayify(getUserOpHash(userOperation, '<entrypoint_address>', 31337))
 
-}).catch((err) => {
-    console.log(err)
-})
+    function getUserOpHash(op, entryPoint, chainId) {
+        const userOpHash = keccak256(packUserOp(op))
+        const enc = ethers.utils.defaultAbiCoder.encode(
+            ['bytes32', 'address', 'uint256'],
+            [userOpHash, entryPoint, chainId])
+        return keccak256(enc)
+    }
 
+    function packUserOp(op) {
+        return ethers.utils.defaultAbiCoder.encode(
+            userOpType,
+            [op.sender, op.nonce, keccak256(op.initCode), keccak256(op.callData),
+                op.callGasLimit, op.verificationGasLimit, op.preVerificationGas, op.maxFeePerGas, op.maxPriorityFeePerGas,
+                keccak256(op.paymasterAndData)])
+    }
+
+    wallet.signMessage(message).then((sig) => {
+        userOperation.signature = sig
+        console.log(userOperation)
+
+        let values = Object.values(userOperation)
+        let str = ""
+        for (let i = 0; i < values.length; i++) {
+            str += values[i]
+            if (i < values.length - 1) {
+                str += ","
+            }
+        }
+        console.log(str)
+
+    }).catch((err) => {
+        console.log(err)
+    })
+});
