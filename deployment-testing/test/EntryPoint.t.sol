cd09a1a0bca711a60d02810bfb1cb0566e31cee0
vishnukvnvv <vishnukvnvv@gmail.com>
2023-07-20 11:41:09 +0530

    Update entry point with helper

--- deployment-testing/test/EntryPoint.t.sol
+++ deployment-testing/test/EntryPoint.t.sol
@@ -1,39 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "./TestHelper.sol";
 import "../src/EntryPoint.sol";
 import "../src/SimpleAccount.sol";
 import "../src/SimpleAccountFactory.sol";
 
-contract EntryPointTest is Test {
-    EntryPoint public ep;
-    SimpleAccountFactory public factory;
-    SimpleAccount public wallet;
-    address payable public walletAddress;
-    address public owner = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+contract EntryPointTest is TestHelper {
 
     function setUp() public {
-        ep = new EntryPoint();
-        factory = new SimpleAccountFactory(ep);
-        wallet = factory.createAccount(owner, 1);
-
-        walletAddress = payable(wallet);
-
+        createAddress("owner_entrypoint");
+        deployEntryPoint(123441);
+        createAccount(123442, 123443);
     }
 
     // Stake Management testing
     // Should deposit for transfer into EntryPoint
     function testDeposit(address signerAddress) public {
-        ep.depositTo{value: 1 ether}(signerAddress);
+        entryPoint.depositTo{value: 1 ether}(signerAddress);
 
-        assertEq(ep.balanceOf(signerAddress), 1 ether);
+        assertEq(entryPoint.balanceOf(signerAddress), 1 ether);
 
-        assertEq(ep.getDepositInfo(signerAddress).deposit, 1 ether);
-        assertEq(ep.getDepositInfo(signerAddress).staked, false);
-        assertEq(ep.getDepositInfo(signerAddress).stake, 0);
-        assertEq(ep.getDepositInfo(signerAddress).unstakeDelaySec, 0);
-        assertEq(ep.getDepositInfo(signerAddress).withdrawTime, 0);
+        assertEq(entryPoint.getDepositInfo(signerAddress).deposit, 1 ether);
+        assertEq(entryPoint.getDepositInfo(signerAddress).staked, false);
+        assertEq(entryPoint.getDepositInfo(signerAddress).stake, 0);
+        assertEq(entryPoint.getDepositInfo(signerAddress).unstakeDelaySec, 0);
+        assertEq(entryPoint.getDepositInfo(signerAddress).withdrawTime, 0);
     }
 
     // Without stake
@@ -41,50 +33,50 @@ contract EntryPointTest is Test {
     function testNoStakeSpecified(uint32 unstakeDelaySec) public {
         if (unstakeDelaySec > 0) {
             vm.expectRevert(bytes("no stake specified"));
-            ep.addStake(unstakeDelaySec);
+            entryPoint.addStake(unstakeDelaySec);
         }
     }
 
     // Should fail to stake without delay
     function testNoDelaySpecified() public {
         vm.expectRevert(bytes("must specify unstake delay"));
-        ep.addStake{value: 1 ether}(0);
+        entryPoint.addStake{value: 1 ether}(0);
     }
 
     // Should fail to unlock
     function testNoStakeUnlock() public {
         vm.expectRevert(bytes("not staked"));
-        ep.unlockStake();
+        entryPoint.unlockStake();
     }
 
     // With stake of 2 eth
     // Should report "staked" state
     function testStakedState(address signerAddress) public {
-        // set msg.sender to specific address
-        vm.prank(signerAddress);
         // add balance to temp address
         vm.deal(signerAddress, 3 ether);
-        ep.addStake{value: 2 ether}(2);
+        // set msg.sender to specific address
+        vm.prank(signerAddress);
+        entryPoint.addStake{value: 2 ether}(2);
 
-        assertEq(ep.getDepositInfo(signerAddress).deposit, 0);
-        assertEq(ep.getDepositInfo(signerAddress).staked, true);
-        assertEq(ep.getDepositInfo(signerAddress).stake, 2 ether);
-        assertEq(ep.getDepositInfo(signerAddress).unstakeDelaySec, 2);
-        assertEq(ep.getDepositInfo(signerAddress).withdrawTime, 0);
+        assertEq(entryPoint.getDepositInfo(signerAddress).deposit, 0);
+        assertEq(entryPoint.getDepositInfo(signerAddress).staked, true);
+        assertEq(entryPoint.getDepositInfo(signerAddress).stake, 2 ether);
+        assertEq(entryPoint.getDepositInfo(signerAddress).unstakeDelaySec, 2);
+        assertEq(entryPoint.getDepositInfo(signerAddress).withdrawTime, 0);
     }
 
     // With deposit
     // Should be able to withdraw
     function testWithdrawDeposit() public {
-        wallet.addDeposit{value: 1 ether}();
+        account.addDeposit{value: 1 ether}();
 
-        assertEq(walletAddress.balance, 0);
-        assertEq(wallet.getDeposit(), 1 ether);
+        assertEq(getAccountBalance(), 0);
+        assertEq(account.getDeposit(), 1 ether);
 
-        vm.prank(owner);
-        wallet.withdrawDepositTo(walletAddress, 1 ether);
+        vm.prank(owner.addr);
+        account.withdrawDepositTo(payable(accountAddress), 1 ether);
 
-        assertEq(address(wallet).balance, 1 ether);
-        assertEq(wallet.getDeposit(), 0);
+        assertEq(getAccountBalance(), 1 ether);
+        assertEq(account.getDeposit(), 0);
     }
 }
