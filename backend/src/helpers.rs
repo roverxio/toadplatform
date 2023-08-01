354814a2c1131c2bbf3ff4e7d69d9af1e889b887
ketan <shubhketanagarwal@99ProblemsButWiFiAint1.local>
2023-08-01 20:09:10 +0530

    get base response

--- backend/src/helpers.rs
+++ backend/src/helpers.rs
@@ -2,10 +2,18 @@ use actix_web::web::Json;
 use serde::Serialize;
 
 use crate::errors::ApiError;
+use crate::models::response::base_response::BaseResponse;
 
-pub fn respond_json<T>(data: T) -> Result<Json<T>, ApiError>
+pub fn respond_json<T>(data: T) -> Result<Json<BaseResponse<T>>, ApiError>
     where
         T: Serialize,
 {
-    Ok(Json(data))
+    Ok(Json(get_base_response(data)))
+}
+
+pub fn get_base_response<T>(data: T) -> BaseResponse<T> where T: Serialize {
+    BaseResponse {
+        data,
+        err: Default::default(),
+    }
 }
