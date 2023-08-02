f8fb23793f59d59dc4f9e984676cdb551e067899
viv-defy <vishnuvardhan@defyclub.com>
2023-08-02 10:30:47 +0530

    modified test_PayForGasUse

--- deployment-testing/test/EntryPoint.t.sol
+++ deployment-testing/test/EntryPoint.t.sol
@@ -146,15 +146,23 @@ contract EntryPointTest is TestHelper {
 
     //account should pay for high gas usage tx
     function test_PayForGasUse() public {
-        UserOperation memory op = fillAndSign(chainId, 0);
+        UserOperation memory op = fillOp(0);
+        uint256 iterations = 45;
+        bytes memory counterCallData = abi.encodeWithSignature("gasWaster(uint256,string)", iterations, '');
+        op.callData = abi.encodeCall(account.execute, (address(counter), 0, counterCallData));
+        op.verificationGasLimit = 1e5;
+        op.callGasLimit = 11e5;
+        op = signUserOp(op, entryPointAddress, chainId);
         userOps.push(op);
-
         entryPoint.depositTo{value: 1 ether}(op.sender);
+
+        uint256 offsetBefore = counter.offset();
         vm.recordLogs();
-        entryPoint.handleOps(userOps, beneficiary);
+        entryPoint.handleOps{gas: 13e5}(userOps, beneficiary);
         Vm.Log[] memory entries = vm.getRecordedLogs();
         (,, uint256 actualGasCost,) = abi.decode(entries[1].data, (uint256, bool, uint256, uint256));
-        assertEq(actualGasCost + entryPoint.getDepositInfo(accountAddress).deposit, 1 ether);
+        assertEq(beneficiary.balance, actualGasCost);
+        assertEq(counter.offset(), offsetBefore + iterations);
     }
 
     //account should not pay if too low gas limit was set
