ee2964f42a1153c04a7d6ea026f24354aed9f97f
ketan <shubhketanagarwal@99ProblemsButWiFiAint1.local>
2023-08-03 16:01:57 +0530

    move struct before impl

--- backend/src/provider/web3_provider.rs
+++ backend/src/provider/web3_provider.rs
@@ -2,11 +2,11 @@ use ethers::{
     providers::{Http, Provider},
 };
 
+pub struct Web3Provider {}
+
 impl Web3Provider {
     pub fn new(chain_url: String) -> Provider<Http> {
         let provider = Provider::try_from(chain_url).unwrap();
         provider
     }
 }
-
-pub struct Web3Provider {}
