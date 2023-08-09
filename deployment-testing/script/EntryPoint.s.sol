31253d5b4c1ba430b140f35d736a8195463a8adc
viv-defy <vishnuvardhan@defyclub.com>
2023-08-09 14:47:22 +0530

    added EntryPoint deployment script

--- deployment-testing/script/EntryPoint.s.sol
+++ deployment-testing/script/EntryPoint.s.sol
@@ -3,15 +3,20 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/EntryPoint.sol";
+import "../src/ImmutableCreate2Factory.sol";
 
 contract EntryPointScript is Script {
     function setUp() public {}
 
     function run() public {
+        // provider is anvil
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
+        // create2 deployer exists on local fork of sepolia
 
-        new EntryPoint{salt: bytes32(uint256(2))}();
+        // gas cannot be used with new
+        EntryPoint entryPoint = new EntryPoint{salt: bytes32(uint256(1))}();
+        console.log("entrypoint addr", address(entryPoint));
 
         vm.stopBroadcast();
     }
