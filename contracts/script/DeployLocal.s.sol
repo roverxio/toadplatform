8338b127dd4161d06edb5c61269c963dfa1ca321
viv-defy <vishnuvardhan@defyclub.com>
2023-08-14 14:50:26 +0530

    hardcoded salt

--- contracts/script/DeployLocal.s.sol
+++ contracts/script/DeployLocal.s.sol
@@ -11,11 +11,6 @@ contract DeployLocal is Script {
     uint256 internal deployerPrivateKey;
     address internal owner;
 
-    uint256 internal epSalt;
-    uint256 internal factorySalt;
-    uint256 internal paymasterSalt;
-    uint256 internal erc20Salt;
-
     EntryPoint internal entryPoint;
     SimpleAccountFactory internal factory;
     VerifyingPaymaster internal paymaster;
@@ -25,26 +20,21 @@ contract DeployLocal is Script {
         deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         owner = address(vm.addr(deployerPrivateKey));
         console.log("=SignerAddress==", owner);
-
-        epSalt = vm.envUint("ENTRYPOINT_SALT");
-        factorySalt = vm.envUint("SIMPLE_ACCOUNT_FACTORY_SALT");
-        paymasterSalt = vm.envUint("VERIFYING_PAYMASTER_SALT");
-        erc20Salt = vm.envUint("TEST_ERC20_SALT");
     }
 
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
         
-        entryPoint = new EntryPoint{salt: bytes32(epSalt)}();
+        entryPoint = new EntryPoint{salt: bytes32(uint256(1))}();
         console.log("=EntryPoint addr==", address(entryPoint));
 
-        factory = new SimpleAccountFactory{salt: bytes32(uint256(factorySalt))}(entryPoint);
+        factory = new SimpleAccountFactory{salt: bytes32(uint256(2))}(entryPoint);
         console.log("=SimpleAccountFactory addr==", address(factory));
 
-        erc20 = new TestERC20{salt: bytes32(erc20Salt)}(18);
+        erc20 = new TestERC20{salt: bytes32(uint256(3))}(18);
         console.log("=TestErC20 addr==", address(erc20));
 
-        paymaster = new VerifyingPaymaster{salt: bytes32(uint256(paymasterSalt))}(entryPoint, owner);
+        paymaster = new VerifyingPaymaster{salt: bytes32(uint256(4))}(entryPoint, owner);
         console.log("=VerifyingPaymaster addr==", address(paymaster));
 
         vm.stopBroadcast();
