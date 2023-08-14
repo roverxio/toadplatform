c9d7a813be83a008062a473c17992aaca4783ee2
viv-defy <vishnuvardhan@defyclub.com>
2023-08-14 14:22:42 +0530

    updated the toad contracts setup script to include anvil

--- contracts/toad_contracts_setup.sh
+++ contracts/toad_contracts_setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-source .env
+anvil &
 
+until curl -s http://localhost:8545 >/dev/null 2>&1; 
+do
+    sleep 1
+done
+
+source .env
 CREATE2_ADDRESS="0x4e59b44847b379578588920cA78FbF26c0B4956C"
 curl http://localhost:8545 -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"anvil_setCode\", \"params\": [\"$CREATE2_ADDRESS\", \"0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3\"]}"
 forge script script/ToadContracts.s.sol:ToadContractsScript --rpc-url $RPC_URL --broadcast
