2cbde88c93f652b05398aab4194bcb542c99aa8d
viv-defy <vishnuvardhan@defyclub.com>
2023-08-18 10:10:23 +0530

    added exit in case cd fails

--- contracts/script/deploy_local.sh
+++ contracts/script/deploy_local.sh
@@ -8,7 +8,7 @@ done
 
 # shellcheck disable=SC2046
 var=$(dirname "$(dirname $(realpath "$0"))")
-cd "${var}"
+cd "${var}" || exit
 source .env
 CREATE2_ADDRESS="0x4e59b44847b379578588920cA78FbF26c0B4956C"
 curl http://localhost:8545 -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"anvil_setCode\", \"params\": [\"$CREATE2_ADDRESS\", \"0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3\"]}" >/dev/null 2>&1;
