fb5bfac62381972f3f18e5825a3a6a185dc5d599
viv-defy <vishnuvardhan@defyclub.com>
2023-08-22 15:20:10 +0530

    reverted changes to db/dao

--- bundler/src/db/dao/mod.rs
+++ bundler/src/db/dao/mod.rs
@@ -1,4 +1,3 @@
 mod connect;
 pub mod transaction_dao;
-pub mod user_op_hash_dao;
 pub mod wallet_dao;
--- bundler/src/db/dao/transaction_dao.rs
+++ bundler/src/db/dao/transaction_dao.rs
@@ -1,23 +1,20 @@
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 
+use crate::db::dao::connect::connect;
+
 #[derive(Clone)]
 pub struct TransactionDao {
     pub pool: Pool<SqliteConnectionManager>,
 }
 
 impl TransactionDao {
-    pub async fn create_user_transaction(
-        &self,
-        _user_address: String,
-        _transaction_id: String,
-        _from_address: String,
-        _to_address: String,
-        _amount: f64,
-        _currency: String,
-        _txn_type: String,
-        _status: String,
-    ) {
-        unimplemented!();
+    pub async fn create_transaction(&self, wallet_address: String, transaction_hash: String) {
+        let conn = connect(self.pool.clone()).await;
+
+        let mut stmt = conn
+            .prepare("INSERT INTO transactions (wallet_address, transaction_hash) VALUES (?, ?)")
+            .unwrap();
+        stmt.execute([wallet_address, transaction_hash]).unwrap();
     }
 }
