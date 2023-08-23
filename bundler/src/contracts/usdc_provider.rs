dce25c9adb28f909e4a158f79d5536f50b6da665
ketan <shubhketanagarwal@99ProblemsButWiFiAint1.local>
2023-08-23 19:13:59 +0530

    expect multiplied value for usdc transfer

--- bundler/src/contracts/usdc_provider.rs
+++ bundler/src/contracts/usdc_provider.rs
@@ -3,6 +3,7 @@ use ethers::abi::Address;
 use ethers::contract::abigen;
 use ethers::providers::{Http, Provider};
 use ethers::types::{Bytes, U256};
+use log::info;
 use std::sync::Arc;
 
 abigen!(ERC20, "abi/ERC20.json");
@@ -20,11 +21,9 @@ impl USDCProvider {
     }
 
     pub fn transfer(&self, to: Address, value: String) -> Result<Bytes, String> {
-        let value: f64 = value.parse().unwrap();
-        let usdc_amount = value * 1e6;
         let data = self
             .abi
-            .transfer(to, U256::from(usdc_amount as u64))
+            .transfer(to, U256::from_dec_str(&value).unwrap())
             .calldata();
         if data.is_none() {
             return Err("transfer data failed".to_string());
