ca53220486e56f3bbc269ecae2ab1b1ccc5b9fe6
viv-defy <vishnuvardhan@defyclub.com>
2023-08-31 10:03:50 +0530

    changed ./Depl... to Depl...

--- contracts/script/deploy_local.sh
+++ contracts/script/deploy_local.sh
@@ -10,4 +10,4 @@ cd $(dirname "$0")
 source ../.env
 CREATE2_ADDRESS="0x4e59b44847b379578588920cA78FbF26c0B4956C"
 curl http://localhost:8545 -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\": \"anvil_setCode\", \"params\": [\"$CREATE2_ADDRESS\", \"0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3\"]}"
-forge script ./DeployLocal.s.sol:DeployLocal --rpc-url $RPC_URL --broadcast
+forge script DeployLocal.s.sol:DeployLocal --rpc-url $RPC_URL --broadcast
