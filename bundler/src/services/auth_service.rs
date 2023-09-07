3777fbf6a85811bb69a35b314df7782beb00f648
vishnukvnvv <vishnukvnvv@gmail.com>
2023-09-07 16:41:52 +0530

    validate firebase id

--- bundler/src/services/auth_service.rs
+++ bundler/src/services/auth_service.rs
@@ -1,4 +1,8 @@
 use jwks_client::keyset::KeyStore;
+use rs_firebase_admin_sdk::{
+    auth::{FirebaseAuthService, UserIdentifiers},
+    App, GcpCredentials,
+};
 use serde::Deserialize;
 
 pub struct AuthService;
@@ -24,5 +28,22 @@ impl AuthService {
                 Err(_) => Err("Failed to decode token".to_string()),
             }
         }
+    }
+
+    pub async fn is_valid_id(verifier_id: String) -> bool {
+        let live_app = App::live(GcpCredentials::new().await.unwrap())
+            .await
+            .unwrap();
+
+        let auth_admin = live_app.auth();
+
+        let user = auth_admin
+            .get_user(
+                // Build a filter for finding the user
+                UserIdentifiers::builder().with_uid(verifier_id).build(),
+            )
+            .await
+            .expect("Error while fetching user");
+        user.is_some()
     }
 }
