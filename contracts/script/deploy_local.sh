748171e633012464a374ba7d3374781730429c09
viv-defy <vishnuvardhan@defyclub.com>
2023-09-07 10:29:56 +0530

    modified the error message

--- contracts/script/deploy_local.sh
+++ contracts/script/deploy_local.sh
@@ -23,11 +23,12 @@ var=$(forge script script/DeployLocal.s.sol:DeployLocal --rpc-url "${RPC_URL}" -
 # loop to check for errors
 while IFS= read -r line; do
   if [[ "$line" == "Error:"* ]]; then
-      echo -e "\033[1;31mError\033[0;31m: Contracts already deployed.\n\033[1;37mHINT\033[0;37m: Kill anvil and run the script again to redeploy the contracts"
+      echo -e "\033[1;31mError\033[0;31m: Contracts already deployed.\n\033[1;37mHINT\033[0;37m: Kill anvil using and run the script again to redeploy the contracts\nTry:\t\t\033[0;0m pkill -f anvil"
       exit 1
   fi
 done <<< "${var}"
 
+echo -e "\033[0;32m== Key= $PRIVATE_KEY"
 # loop to look for and output the relevant addresses that are created/used in forge script
 while IFS= read -r line; do
   if [[ "$line" == *"addr="* ]]; then
