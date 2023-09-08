065030e17048b996f97a3285765d12d945e66c43
viv-defy <vishnuvardhan@defyclub.com>
2023-09-08 17:23:35 +0530

    added DB initialization

--- sync-pipelines-rust/src/db/connection.rs
+++ sync-pipelines-rust/src/db/connection.rs
@@ -1,9 +1,20 @@
-use sqlx::{Pool, Postgres};
+use std::process::exit;
+use log::warn;
+use sqlx::{PgPool, Postgres};
 
 pub struct Connection {}
 
 impl Connection {
-    pub fn init() -> Pool<Postgres> {
-        unimplemented!()
+    pub async fn init() -> sqlx::Pool<Postgres> {
+        let database_url: String;
+        match std::env::var("DATABASE_URL") {
+            Ok(url) => database_url = url,
+            Err(_) => {
+                warn!("env DATABASE_URL not set");
+                exit(1)
+            }
+        }
+        let connection = PgPool::connect(&database_url).await;
+        connection.unwrap()
     }
 }
