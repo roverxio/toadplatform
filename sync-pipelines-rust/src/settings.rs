c1ad2dfa2fac50e4698a93587b045b3e6794fbbf
viv-defy <vishnuvardhan@defyclub.com>
2023-09-11 17:13:26 +0530

    modified the getters' return types in Settings

--- sync-pipelines-rust/src/settings.rs
+++ sync-pipelines-rust/src/settings.rs
@@ -1,11 +1,10 @@
-use bigdecimal::num_bigint::BigInt;
 use config::{Config, ConfigError, File};
 use serde::Deserialize;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Deserialize)]
 pub struct LastSyncBlock {
-    start_time: StartTime,
+    start_time: StartBlock,
     files: SyncFiles,
 }
 
@@ -17,7 +16,7 @@ pub struct SyncFiles {
 
 #[derive(Deserialize)]
 pub struct StartBlock {
-    token_transfers: BigInt,
+    token_transfers: u64,
     transactions: String,
 }
 
@@ -34,37 +33,37 @@ const CONFIG_FILE_PATH: &str = "./Config.toml";
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
         let s = Config::builder()
-            .add_source(File::with_name(CONFIG_FILE_PATH).required(false))
+            .add_source(File::with_name(CONFIG_FILE_PATH).required(true))
             .build()?;
 
         s.try_deserialize()
     }
 
-    pub fn get_last_sync_file_token_transfers(&self) -> &PathBuf {
+    pub fn get_last_sync_file_token_transfers(&self) -> &Path {
         &self.last_sync_time.files.token_transfers
     }
 
-    pub fn get_last_sync_file_transactions(&self) -> &PathBuf {
+    pub fn get_last_sync_file_transactions(&self) -> &Path {
         &self.last_sync_time.files.transactions
     }
 
-    pub fn get_last_sync_block_token_transfers(&self) -> &BigInt {
-        &self.last_sync_time.start_time.token_transfers
+    pub fn get_last_sync_block_token_transfers(&self) -> u64 {
+        self.last_sync_time.start_time.token_transfers.clone()
     }
 
-    pub fn get_last_sync_time_transactions(&self) -> &String {
+    pub fn get_last_sync_time_transactions(&self) -> &str {
         &self.last_sync_time.start_time.transactions
     }
 
-    pub fn get_chain(&self) -> &String {
+    pub fn get_chain(&self) -> &str {
         &self.chain
     }
 
-    pub fn get_native_currency(&self) -> &String {
+    pub fn get_native_currency(&self) -> &str {
         &self.native_currency
     }
 
-    pub fn get_transaction_id_prefix(&self) -> &String {
+    pub fn get_transaction_id_prefix(&self) -> &str {
         &self.transaction_id_prefix
     }
 }
