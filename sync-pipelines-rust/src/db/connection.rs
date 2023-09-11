d5603eb71b4446f64218ce0a519016e99bed86da
viv-defy <vishnuvardhan@defyclub.com>
2023-09-11 15:56:36 +0530

    in DB connection- modified Ok block; changed warn to error

--- sync-pipelines-rust/src/db/connection.rs
+++ sync-pipelines-rust/src/db/connection.rs
@@ -1,4 +1,4 @@
-use log::warn;
+use log::error;
 use sqlx::{PgPool, Pool, Postgres};
 use std::process::exit;
 
@@ -8,13 +8,15 @@ impl Connection {
     pub async fn init() -> Pool<Postgres> {
         let database_url: String;
         match std::env::var("DATABASE_URL") {
-            Ok(url) => database_url = url,
+            Ok(url) => {
+                database_url = url;
+                let connection = PgPool::connect(&database_url).await;
+                connection.unwrap()
+            }
             Err(_) => {
-                warn!("env DATABASE_URL not set");
+                error!("env DATABASE_URL not set");
                 exit(1)
             }
         }
-        let connection = PgPool::connect(&database_url).await;
-        connection.unwrap()
     }
 }
