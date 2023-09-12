0a4144f00e45d3f26120739bd161cb5ed13672f4
viv-defy <vishnuvardhan@defyclub.com>
2023-09-12 13:02:55 +0530

    added and modified last synced functions

--- sync-pipelines-rust/src/utils/utils.rs
+++ sync-pipelines-rust/src/utils/utils.rs
@@ -2,11 +2,13 @@ use crate::CONFIG;
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 
 pub struct Utils {}
 
 impl Utils {
-    pub fn get_last_synced_time() -> i64 {
+    pub fn get_last_synced_block_timestamp() -> i64 {
         let result = fs::read_to_string(CONFIG.get_last_sync_file_transactions());
         match result {
             Ok(time) => time.parse::<i64>().unwrap(),
@@ -17,18 +19,26 @@ impl Utils {
     pub fn get_last_synced_block_number() -> i64 {
         let result = fs::read_to_string(CONFIG.get_last_sync_file_transactions());
         match result {
-            Ok(number) => number.parse::<i64>().unwrap(),
+            Ok(number) => {
+                println!("{}", number);
+                number.parse::<i64>().unwrap()
+            }
             Err(_) => CONFIG.get_last_sync_block_token_transfers(),
         }
     }
 
-    pub fn update_last_synced_time(_last_sync_time: i64) {
-        // update the last sync time for the table
-        unimplemented!();
+    pub fn update_last_synced_time(last_sync_time: i64) {
+        let mut file = File::create(CONFIG.get_last_sync_file_transactions())
+            .expect("Unable to create last sync file token_transfers");
+        file.write(last_sync_time.to_string().as_bytes())
+            .expect("Unable to write to last sync file token_transfers");
     }
 
-    pub fn update_last_synced_block(_last_synced_block: i64) {
-        unimplemented!()
+    pub fn update_last_synced_block(last_sync_block: i64) {
+        let mut file = File::create(CONFIG.get_last_sync_file_token_transfers())
+            .expect("Unable to create last sync file token_transfers");
+        file.write(last_sync_block.to_string().as_bytes())
+            .expect("Unable to write to last sync file token_transfers");
     }
 
     pub fn generate_txn_id() -> String {
