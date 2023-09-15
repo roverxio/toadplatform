f0ce988cf76795ee421bf001e12f8c3e92503e71
viv-defy <vishnuvardhan@defyclub.com>
2023-09-15 11:43:11 +0530

    changed references of time/number to block

--- sync-pipelines-rust/src/models/last_sync.rs
+++ sync-pipelines-rust/src/models/last_sync.rs
@@ -9,7 +9,7 @@ impl LastSync {
     pub fn get_last_synced_block_transactions() -> i64 {
         let result = fs::read_to_string(CONFIG.get_last_sync_file_transactions());
         match result {
-            Ok(time) => time.parse::<i64>().unwrap(),
+            Ok(block) => block.parse::<i64>().unwrap(),
             Err(_) => CONFIG.get_last_sync_block_transactions(),
         }
     }
@@ -17,15 +17,15 @@ impl LastSync {
     pub fn get_last_synced_block_token_transfers() -> i64 {
         let result = fs::read_to_string(CONFIG.get_last_sync_file_token_transfers());
         match result {
-            Ok(number) => number.parse::<i64>().unwrap(),
+            Ok(block) => block.parse::<i64>().unwrap(),
             Err(_) => CONFIG.get_last_sync_block_token_transfers(),
         }
     }
 
-    pub fn update_last_synced_block_transactions(last_sync_time: i64) {
+    pub fn update_last_synced_block_transactions(last_sync_block: i64) {
         let mut file = File::create(CONFIG.get_last_sync_file_transactions())
             .expect("Unable to create last sync file token_transfers");
-        file.write(last_sync_time.to_string().as_bytes())
+        file.write(last_sync_block.to_string().as_bytes())
             .expect("Unable to write to last sync file token_transfers");
     }
 
