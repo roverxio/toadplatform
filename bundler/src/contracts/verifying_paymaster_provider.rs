09383449bfc84a0f40356fe0a30b1fa74b624390
vishnukvnvv <vishnukvnvv@gmail.com>
2023-10-04 16:17:34 +0530

    add paymaster initialization to client

--- bundler/src/contracts/verifying_paymaster_provider.rs
+++ bundler/src/contracts/verifying_paymaster_provider.rs
@@ -1,7 +1,11 @@
 use ethers::contract::abigen;
 use ethers::providers::{Http, Provider};
 use ethers::types::Address;
+use ethers::utils::format_ether;
+use log::error;
 use std::sync::Arc;
+
+use crate::provider::web3_client::Web3Client;
 
 abigen!(VerifyingPaymaster, "abi/VerifyingPaymaster.json");
 
@@ -14,5 +18,20 @@ impl VerifyingPaymasterProvider {
     ) -> VerifyingPaymaster<Provider<Http>> {
         let contract: VerifyingPaymaster<Provider<Http>> = VerifyingPaymaster::new(address, client);
         contract
+    }
+
+    pub async fn get_deposit(client: &Web3Client) -> Result<String, String> {
+        let response = client
+            .get_verifying_paymaster_provider()
+            .get_deposit()
+            .await;
+        if response.is_err() {
+            error!(
+                "Paymaster: Deposit: {:?}",
+                response.err().unwrap().to_string()
+            );
+            return Err(String::from("Failed to get balance"));
+        }
+        Ok(format_ether(response.unwrap()))
     }
 }
