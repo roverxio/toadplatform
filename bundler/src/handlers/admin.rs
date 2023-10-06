0dabd982b3155183a21dcba0aa9db900e8ba249e
vishnukvnvv <vishnukvnvv@gmail.com>
2023-10-06 15:14:17 +0530

    update pending imports

--- bundler/src/handlers/admin.rs
+++ bundler/src/handlers/admin.rs
@@ -3,8 +3,7 @@ use actix_web::{HttpRequest, HttpResponse};
 use sqlx::{Pool, Postgres};
 
 use crate::errors::AdminError;
-use crate::models::admin::AddMetadataRequest;
-use crate::models::admin::PaymasterTopup;
+use crate::models::admin::{AddMetadataRequest, PaymasterTopup};
 use crate::models::response::base_response::BaseResponse;
 use crate::models::wallet::BalanceRequest;
 use crate::provider::helpers::get_user;
