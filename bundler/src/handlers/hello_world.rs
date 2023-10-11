03d30483135571273f567a4ba0f6dff255c76276
vishnukvnvv <vishnukvnvv@gmail.com>
2023-10-11 13:11:02 +0530

    test cases added

--- bundler/src/handlers/hello_world.rs
+++ bundler/src/handlers/hello_world.rs
@@ -7,3 +7,22 @@ pub async fn hello_world() -> Result<HttpResponse, Error> {
     let hello = HelloWorldService::hello_world()?;
     Ok(HttpResponse::Ok().json(BaseResponse::init(hello)))
 }
+
+#[cfg(test)]
+mod test {
+    use actix_web::{test, web, App};
+    use reqwest::StatusCode;
+
+    use crate::handlers::hello_world::hello_world;
+
+    #[actix_web::test]
+    async fn test_hello_world() {
+        let app = test::init_service(
+            App::new().service(web::scope("v1").route("hello", web::get().to(hello_world))),
+        )
+        .await;
+        let req = test::TestRequest::get().uri("/v1/hello").to_request();
+        let result = test::call_service(&app, req).await;
+        assert_eq!(result.status(), StatusCode::OK);
+    }
+}
--- bundler/src/services/hello_world_service.rs
+++ bundler/src/services/hello_world_service.rs
@@ -12,3 +12,20 @@ impl HelloWorldService {
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::models::HelloWorld;
+    use crate::services::HelloWorldService;
+
+    #[actix_web::test]
+    async fn test_hello_world_service() {
+        let data = HelloWorldService::hello_world().unwrap();
+        assert_eq!(
+            data,
+            HelloWorld {
+                name: "Hello world!".to_string()
+            }
+        )
+    }
+}
